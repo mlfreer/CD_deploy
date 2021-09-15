9c8310155647a28ca59a1ac96119565ff3e21275
mlfreer <1991F@inbox.ru>
2021-09-15 14:26:07 +0100

    upscaling the rounds for the experiment

--- collective_deliberation_infoTreatment/models.py
+++ collective_deliberation_infoTreatment/models.py
@@ -16,9 +16,10 @@ doc = "The experiment consists of 15 rounds. Before making any decision, subject
 class Constants(BaseConstants):
     name_in_url = 'collective_deliberation_infoTreatment'
     players_per_group = 3
-    num_rounds = 2
+    num_rounds = 15
 
-    endowment = cu(100)
+# not sure why do we need the endowment thingy?
+#    endowment = cu(100)
 
     multiplier = 1.8
     disclose_cost = 5
